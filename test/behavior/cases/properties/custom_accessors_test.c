c250d258a5cd668a93f2d115e002a849cfab7798
Rodrigo Peixoto <rodrigopex@gmail.com>
2026-03-10 07:50:36 -0300

    test(transpiler): add behavior test for custom getter/setter and @synthesize ivar names
    Tests custom getter (getter=isEnabled), custom setter (setter=applySpeed:),
    and custom ivar names via @synthesize foo = _bar.

---
+++ test/behavior/cases/properties/custom_accessors_test.c
@@ -0,0 +1,20 @@
+/* Behavior test: custom getter/setter names and custom ivar via @synthesize */
+#include "unity.h"
+#include "Switch.h"
+#include "oz_mem_slabs.h"
+
+void test_custom_getter_name(void)
+{
+	struct Switch *sw = Switch_alloc();
+	Switch_setEnabled_(sw, 1);
+	TEST_ASSERT_EQUAL_INT(1, Switch_isEnabled(sw));
+	OZObject_release((struct OZObject *)sw);
+}
+
+void test_custom_setter_name(void)
+{
+	struct Switch *sw = Switch_alloc();
+	Switch_applySpeed_(sw, 50);
+	TEST_ASSERT_EQUAL_INT(50, Switch_speed(sw));
+	OZObject_release((struct OZObject *)sw);
+}
