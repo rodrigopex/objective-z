4bee57583f08bc270333cd3ee2534eb89b3ecdcd
Rodrigo Peixoto <rodrigopex@gmail.com>
2026-03-25 14:54:13 -0300

    samples: add precision format demo to transpiled_literals
    Show %.N@ format specifier with 1/3 at various precisions.

--- samples/transpiled_literals/src/main.m
+++ samples/transpiled_literals/src/main.m
@@ -14,11 +14,20 @@ int main(void)
 
 	/* Q31 fixed point numbers literals */
 	OZQ31 *a = @10;
-	OZQ31 *b = @250;
+	OZQ31 *b = @25002031;
 	OZLog("a = %@, b = %@, a + b = %@", a, b, [a add:b]);
 	OZLog("a = %@, b = %@, a - b = %@", a, b, [a sub:b]);
 	OZLog("a = %@, b = %@, a * b = %@", a, b, [a mul:b]);
-	OZLog("a = %@, b = %@, a / b = %@", a, b, [a div:b]);
+	OZLog("a = %@, b = %@, a / b = %.10@", a, b, [a div:b]);
+
+	/* Precision demo: 1/3 has full Q31 precision */
+	OZQ31 *one = @1;
+	OZQ31 *three = @3;
+	OZQ31 *third = [one div:three];
+	OZLog("1/3 default %%@:   %@", third);
+	OZLog("1/3 with %%.4@:    %.4@", third);
+	OZLog("1/3 with %%.10@:   %.10@", third);
+	OZLog("1/3 with %%.14@:   %.14@", third);
 
 	/* String literals */
 	OZString *greeting = @"hello";
